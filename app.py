782ec54eea85bd403d78b0e5453da368bbc571ea
SeanGau <sean@b1t.tw>
2021-09-03 21:36:18 +0800

    init

---
+++ app.py
@@ -0,0 +1,82 @@
+import flask
+import os
+import csv
+import shutil
+import threading
+from string import Template
+from datetime import datetime
+from flask_mail import Mail, Message
+from markdown import markdown
+
+app = flask.Flask(__name__)
+app.config.from_object('config')
+mail = Mail()
+
+path = os.getcwd()
+ALLOWED_EXTENSIONS = set(['csv', 'pdf'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    @flask.copy_current_request_context
+    def send_mail(recevier_array, subject, html_content, upload_folder, attachment):
+        try:
+            msg = Message(subject=subject, recipients=recevier_array, html=html_content)
+            
+            if attachment is not None:
+                if ".pdf" not in attachment:
+                    attachment += ".pdf"
+                with app.open_resource(os.path.join(upload_folder, attachment)) as fp:  
+                    msg.attach(attachment, "application/pdf", fp.read())
+            print(msg)
+            mail.send(msg)
+            success.append({"recevier": recevier_array[0], "subject": subject})
+        except:
+            error.append({"recevier": recevier_array[0], "subject": subject})
+    error = []
+    success = []
+    threads = []
+    if flask.request.method == 'POST':
+        UPLOAD_FOLDER = os.path.join(path, 'uploads'+str(datetime.now().timestamp()))
+        if not os.path.isdir(UPLOAD_FOLDER):
+            os.mkdir(UPLOAD_FOLDER)
+        _data = flask.request.form.to_dict()
+        app.config['MAIL_USERNAME'] = _data.get('email')
+        app.config['MAIL_PASSWORD'] = _data.get('apppw')
+        app.config['MAIL_DEFAULT_SENDER'] = (_data.get('name'), _data.get('email'))
+        mail.init_app(app)
+        csvfile = flask.request.files['csv']
+        if 'files[]' in flask.request.files:
+            files = flask.request.files.getlist('files[]')
+            for file in files:
+                if file and allowed_file(file.filename):
+                    filename = file.filename
+                    file.save(os.path.join(UPLOAD_FOLDER, filename))
+        if csvfile and allowed_file(csvfile.filename):
+            filename = csvfile.filename
+            csvfile.save(os.path.join(UPLOAD_FOLDER, filename))
+            with open(os.path.join(UPLOAD_FOLDER, csvfile.filename), encoding='utf-8', newline='') as csvfile_saved:
+                reader = csv.DictReader(csvfile_saved)
+                _data['template'] = markdown(_data['template'].replace("\r\n","<br>"))
+                for row in reader:
+                    content_tmpl = Template(_data['template'])
+                    attachment = row.get('attachment', None)
+                    t = threading.Thread(target = send_mail,args=([row['email']], row['subject'], content_tmpl.substitute(row), UPLOAD_FOLDER, attachment))
+                    t.start()
+                    threads.append(t)
+        for t in threads:
+            t.join()
+        try:
+            shutil.rmtree(UPLOAD_FOLDER)
+        except OSError as e:
+            print("Error: %s : %s" % (UPLOAD_FOLDER, e.strerror))
+        return flask.render_template('finish.html', success = success, error = error)
+    else:
+        return flask.render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000, debug=True)
