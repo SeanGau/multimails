4ea602dc1ce04d9d9a46f9ef1ca762256e8abe67
SeanGau <rrtw0627@gmail.com>
2025-12-20 20:40:11 +0800

    update

--- app.py
+++ app.py
@@ -40,10 +40,16 @@ def index():
     success = []
     threads = []
     if flask.request.method == 'POST':
-        UPLOAD_FOLDER = os.path.join(path, 'uploads'+str(datetime.now().timestamp()))
+        UPLOAD_FOLDER: str = os.path.join(path, 'uploads'+str(datetime.now().timestamp()))
         if not os.path.isdir(UPLOAD_FOLDER):
             os.mkdir(UPLOAD_FOLDER)
         _data = flask.request.form.to_dict()
+        
+        # Configure SMTP settings from user input
+        app.config['MAIL_SERVER'] = _data.get('smtp_server')
+        app.config['MAIL_PORT'] = int(_data.get('smtp_port', 587))
+        app.config['MAIL_USE_TLS'] = _data.get('smtp_use_tls') == 'on'
+        app.config['MAIL_USE_SSL'] = False  # Set to True if using port 465
         app.config['MAIL_USERNAME'] = _data.get('email')
         app.config['MAIL_PASSWORD'] = _data.get('apppw')
         app.config['MAIL_DEFAULT_SENDER'] = (_data.get('name'), _data.get('email'))
@@ -54,11 +60,11 @@ def index():
             for file in files:
                 if file and allowed_file(file.filename):
                     filename = file.filename
-                    file.save(os.path.join(UPLOAD_FOLDER, filename))
+                    file.save(os.path.join(UPLOAD_FOLDER, filename)) # type: ignore
         if csvfile and allowed_file(csvfile.filename):
             filename = csvfile.filename
-            csvfile.save(os.path.join(UPLOAD_FOLDER, filename))
-            with open(os.path.join(UPLOAD_FOLDER, csvfile.filename), encoding='utf-8', newline='') as csvfile_saved:
+            csvfile.save(os.path.join(UPLOAD_FOLDER, filename)) # type: ignore
+            with open(os.path.join(UPLOAD_FOLDER, csvfile.filename), encoding='utf-8', newline='') as csvfile_saved: # type: ignore
                 reader = csv.DictReader(csvfile_saved)
                 _data['template'] = markdown(_data['template'].replace("\r\n","<br>"))
                 for row in reader:
